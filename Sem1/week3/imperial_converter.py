ee069b04b4210723a287ab8dc5e37ebfd9dbf145
Yumi Verma <yumiverma@lilly.staff.staffs.ac.uk>
2022-10-27 11:20:42 +0100

    finished_imperial_converter

--- Sem1/week3/imperial_converter.py
+++ Sem1/week3/imperial_converter.py
@@ -1,1 +1,30 @@
-import math
+#error handeling
+valid = "false"
+while valid  == "false":
+	try:
+		feet = int(input("Input height in Feet: "))
+		inches = int(input("Input height in Inches: "))
+		valid = "true"
+
+
+	except:
+		print ("Try Again")
+
+#cm conversion
+
+cm1 = feet * 12
+cm2 = cm1 + inches
+cm = cm2 * 2.54
+
+#calculation for diffrent converions.
+km = cm / 100000
+print ("Height in Kilometers: ",km)
+
+m = cm / 100
+print ("Height in Meteres: ",m)
+
+#cm print
+print ("Height in Centimeteres: ",cm)
+
+mm = cm * 10
+print ("Height in Milimeteres: ",mm)
