155d7f476a7be8de1dd4a276044853ea78f0e348
Yumi Verma <yumiverma@Lilly.local>
2022-10-27 15:20:42 +0100

    Started_on_bandwith_task

---
+++ Sem1/week3/bandwith.py
@@ -0,0 +1,15 @@
+import sys
+import math
+program_validity = "false"
+
+while program_validity ==  "false":
+	try:
+		UserMbpsInput =  int(input("Input Bandwith limit in Mbps: "))
+		UsersRequired = int(input("Input Users Required: "))
+		break
+
+	except:
+		print ("Unexpected error, Try Again!")
+
+Usersbps = UserMbpsInput * 1000000
+print ("Your Bandwith in bps is", Userbps)
