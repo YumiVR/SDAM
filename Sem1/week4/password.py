e5e019ec0b4b9e32d9838707bfd74cfe68f83d5c
Yumi Verma <yumiverma@lilly.staff.staffs.ac.uk>
2022-11-10 10:34:04 +0000

    backup

--- Sem1/week4/password.py
+++ Sem1/week4/password.py
@@ -1,1 +1,20 @@
 
+valid = "false"
+
+passwd = "rocket"
+
+while valid == "false":
+	try:
+		passwd_entered = input("Please input your password: ")
+		
+		if passwd_entered == passwd:
+			print ("Welcome User")
+			valid = "true"
+			
+
+		else:
+			print ("Wrong password, Try Again!")	
+
+	except:
+		print("Please Try Again!")
+
