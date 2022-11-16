26b35522c034996ef329f03e524d1d1b563645f3
Yumi Verma <yumiverma@Lilly.local>
2022-11-16 10:53:15 +0000

    finished_magpie

---
+++ Sem1/week4/magpie.py
@@ -0,0 +1,28 @@
+import sys
+print ("welcome to the program")
+print ("press enter to exit the program")
+
+valid = "false"
+
+song = {
+	1 : "One for Sorrow",
+	2 : "Two for Joy",
+	3 : "Three for a Girl",
+	4 : "Four for a Boy",
+	5 : "Five for Silver",
+	6 : "Six for Gold",
+	7 : "Seven for a secret never to be to"
+}
+
+while valid == "false":
+
+	try:
+		print(song[int(input("Enter a number: "))])
+	except:
+		print("Not a permited number")
+		print("would you like to exit?")
+		userinput = input("(y) for Yes or (n) for No > ")
+		if userinput == 'y':
+			sys.exit()
+		elif userinput == 'n':
+			print("No Exit")
